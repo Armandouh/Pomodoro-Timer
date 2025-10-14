bef8fc1e9d1997503db5099fe367a8d453a81705
Arman Aslanyan <armo2004.16@gmail.com>
2025-10-14 09:57:15 +0400

    Initial commit, added simple timer layout + viewModel

---
+++ PomodoroTimer/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace PomodoroTimer.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    [ObservableProperty] private int _minutes = 25;
+    [ObservableProperty] private int _seconds = 0;
+    [ObservableProperty] private string _timeToDisplay = "25:00";
+
+    [RelayCommand]
+    private void Start()
+    {
+        Console.WriteLine("Timer started!");
+    }
+    
+    [RelayCommand]
+    private void Pause()
+    {
+        Console.WriteLine("Timer paused!");
+    }
+
+    [RelayCommand]
+    private void Skip()
+    {
+        Console.WriteLine("Timer skipped!");
+    }
+}
---
+++ PomodoroTimer/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace PomodoroTimer.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
